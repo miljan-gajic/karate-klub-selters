fb610b64b5efb78ee0350b2718f9f62b23dc5a3d
miljan-gajic <miljan.gajic985@gmail.com>
2021-10-31 22:09:39 +0100

    feat: Adding authentication using jwt

---
+++ models/Users.ts
@@ -0,0 +1,15 @@
+import { Schema, model, models } from 'mongoose'
+import { User } from 'types/User'
+
+const UserSchema = new Schema<User>(
+  {
+    password: { type: String, required: true },
+    username: { type: String, required: true },
+    role: String,
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export default models.User || model<User>('User', UserSchema)
---
+++ pages/api/users/register.ts
@@ -0,0 +1,50 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import bcryptjs from 'bcryptjs'
+import mongoose from 'mongoose'
+import User from '../../../models/Users'
+import dbConnect from '../../../lib/mongodb'
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  switch (req.method) {
+    case 'POST':
+      return register(req, res)
+
+    default:
+      return res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
+
+  async function register(req: NextApiRequest, res: NextApiResponse) {
+    await dbConnect()
+    let { username, password } = req.body
+
+    bcryptjs.hash(password, 10, (hashError, hash) => {
+      if (hashError) {
+        return res.status(500).json({
+          message: hashError.message,
+          error: hashError,
+        })
+      }
+      const user = new User({
+        _id: new mongoose.Types.ObjectId(),
+        username,
+        password: hash,
+      })
+
+      return user
+        .save()
+        .then((user) => {
+          return res.status(201).json({
+            user,
+          })
+        })
+        .catch((error) => {
+          res.status(500).json({
+            message: error.message,
+            error,
+          })
+        })
+    })
+  }
+}
+
+export default handler
