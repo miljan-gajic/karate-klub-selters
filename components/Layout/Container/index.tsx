a697defad0b046ecd32412dd9c37e394bb486cf0
Miljan Gajic <miljan.gajic@bsdex.de>
2021-11-17 21:25:59 +0100

    feat: Register Page --- cleanup

--- components/Layout/Container/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from 'antd'
-import React from 'react'
-
-import Styles from './styles.module.sass'
-
-export default function Container({ children }) {
-  const { Header, Content, Sider, Footer } = Layout
-  return <Layout className={Styles.mainContainer}>{children}</Layout>
-}
