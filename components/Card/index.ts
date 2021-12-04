9b343a8a1cd7ad7d1ed45a5c9d776527dd82a240
miljan-gajic <miljan.gajic985@gmail.com>
2021-12-04 19:05:13 +0100

    feat: Adding a sign_up - register page

---
+++ components/Card/index.ts
@@ -0,0 +1,14 @@
+import { styled } from 'styled-components'
+
+export const CardWrapper = styled.div`
+  color: #fff;
+  backgound-image: ${({ image }) => image};
+  background-size: cover;
+  padding: 10rem 0 0;
+`
+
+export const CardTitle = styled.h2``
+
+export const CardBody = styled.p``
+
+export const CardActionButton = styled.a``
